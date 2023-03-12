dd5157f8904a1fa8954a92a65268500b4cfa127c
Isaac Ndarwa <isaac.ndarwa@copiakenya.com>
2023-03-13 00:32:52 +0300

    Created Registration Form

--- crm/crm_app/views.py
+++ crm/crm_app/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import RegisterForm
 
-# Create your views here.
+def home(request):
+    # Check if user logging in, or already logged in
+    # If logging in, POST req. If logged in, GET req.
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate with django
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged in Successfully")
+            return redirect('home')
+        else:
+            messages.error(request, "Wrong Username or Password")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+# def login_user(request):
+#     pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out")
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate & login the new user
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Welcome. You have Registered Successfully")
+            return redirect('home')
+    else:
+        form = RegisterForm()
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
