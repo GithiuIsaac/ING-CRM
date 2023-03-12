a2e6196481b298551da659d307a8b546bc094395
Isaac Ndarwa <isaac.ndarwa@copiakenya.com>
2023-03-13 00:55:31 +0300

    Added & Registered Record Model

--- crm/crm_app/admin.py
+++ crm/crm_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Record
 
-# Register your models here.
+# Register models.
+admin.site.register(Record)
--- crm/crm_app/models.py
+++ crm/crm_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    # Automatically add the time at which record is created
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    county = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return(f"{self.first_name} {self.last_name}")
